a4b202def60080133e7b278f40587cd645060fdf
Egor <egor@mail.ru>
2019-06-19 21:18:07 +0300

    First

---
+++ Lessons6.js
@@ -0,0 +1,9 @@
+/* Объекты
+o.name
+person['age'] - обращение к объекту.
+o.sayHello();
+o['sayHello']();
+this в момент вызова функции;
+Функция конструктор let o = new Object; слово new;
+return в конструкторе не нужен; конструкторы с большой буквы; нужен для создание объекта.
+new Function\ new Number\ new Array\
