eb76be530f99e0f60256bdd705825f516d87d87d
unknown <egorc@mail.ru>
2019-07-24 21:20:32 +0300

    AddEvent

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const root = document.getElementById('root');
 
 const divWrapper = document.createElement('div');
 const input = document.createElement('input');
+const input1 = document.createElement('input');
 const h1 = document.createElement('h1');
 const divBtnWrapper = document.createElement('div');
 const btnSave = document.createElement('button');
@@ -10,39 +11,53 @@ btnSave.textContent = 'Save'; // почему не .value ??? // btnSave.innerHT
 btnCansel.textContent = 'Cansel'; //btnCansel.appendChild(document.createTextNode('Egor'))
 h1.innerText = 'Введите пароль';
 
+document.body.style.backgroundColor = 'black';
 divWrapper.className = 'wrapper';
 divBtnWrapper.className = 'button-wrapper';
 btnSave.className = 'btn-save';
 btnCansel.className = 'btn-cansel';
-
+input1.setAttribute('type' , 'color')
+input1.setAttribute('value' , '#e66465')
 // btnCansel.setAttribute('button' , 'Cansel');
 divWrapper.appendChild(h1);
 divWrapper.appendChild(input);
+divWrapper.appendChild(input1);
 divWrapper.appendChild(divBtnWrapper);
 divBtnWrapper.appendChild(btnSave);
 divBtnWrapper.appendChild(btnCansel);
 root.appendChild(divWrapper);
+// root.insertAdjacentElement
+// const defaultColor = document.body.style.backgroundColor;
+// defaultColor = ' ';
+function handleSave(e) {
+   document.body.style.backgroundColor =  input1.value;
+}
+
+function handleReset(e) {
+  document.body.style.backgroundColor = ' ';
+  // document.body.style.backgroundColor = defaultColor;
+}
+
+btnSave.addEventListener('click', handleSave);
+btnCansel.addEventListener('click', handleReset);
 
 
+// let colorB = getComputedStyle(document.body);
 
+// setTimeout (function() {
+//   let userQ = prompt("Какой цвет фона установить");
+//   document.body.style.backgroundColor = userQ;
+//   const textForUser = document.createElement('h2');
+//   textForUser.innerText = 'Цвет фона был установлен ' + userQ;
+//   divWrapper.appendChild(textForUser); 
+//   setTimeout (function() {
+//     document.body.style.backgroundColor = 'white';
+//       // defaultCol = 'white';
+//       divWrapper.removeChild(divWrapper.lastChild);
+// }, 3000);
+//   }, 200);
 
-
-let colorB = getComputedStyle(document.body);
-
-setTimeout (function() {
-  let userQ = prompt("Какой цвет фона установить");
-  document.body.style.backgroundColor = userQ;
-  const textForUser = document.createElement('h2');
-  textForUser.innerText = 'Цвет фона был установлен ' + userQ;
-  divWrapper.appendChild(textForUser); 
-  setTimeout (function() {
-    document.body.style.backgroundColor = 'white';
-      // defaultCol = 'white';
-      divWrapper.removeChild(divWrapper.lastChild);
-}, 3000);
-  }, 200);
-
-console.log(colorB.color);
+// console.log(colorB.color);
 
 
 
@@ -67,3 +82,4 @@ console.log(colorB.color);
 // 3 УСТАНАВЛИВАЕМ ЦВЕТ ФОНА
 // 4 через 5 секунд возвращаем дефолтное значение
 // 5* Вставить сообщение перед фоном о том что цвет бекграунда поменялся на (тот который был указан пользователем)
+// прочитать про this
---
+++ lesson11.js
@@ -0,0 +1,31 @@
+// гугл и яндекс метрика
+// обработчик события
+// навигатор + онклик
+
+// 2- споссоб
+// function showAlert(){
+//     alert('Hello')
+// }
+// const button = document.querySelector('button');
+// button.onclick = showAlert
+
+// 3-й принимает три переменных
+// button.addEventListener('click', showAlert, false) - true- вызовеь в момент погружения\
+//  false в момент всплытия- по умолчанию стоит
+//  button.addEventListener('click', showAlert1, false) \\ (e) => {if (e.target.innerHTML === 'Click me1')}
+//  button.addEventListener('click', showAlert2, false)
+
+//  button.removeEventListener('click', showAlert3, 
+
+//<input type="color" value="#ff0000">
+
+// const root = document.getElementById('root');
+// const divWrapper = document.createElement('div');
+// const input = document.createElement('input');
+// const btnSave = document.createElement('button');
+// const btnCansel = document.createElement('button');
+
+// input.setAttribute('type');
+// divWrapper.appendChild(input);
+// root.appendChild(divWrapper);
+
